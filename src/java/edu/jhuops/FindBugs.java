20c824807d924b0992fc203ce79022dbeb40e22b
teena <teena@cois4u.com>
2023-08-01 19:38:47 -0400

    a

---
+++ src/java/edu/jhuops/FindBugs.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.jhuops;
+
+/**
+ *
+ * @author teena
+ */
+public class FindBugs {
+
+    private String lastname;
+
+    /**
+     * Get the value of lastname
+     *
+     * @return the value of lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @param lastname new value of lastname
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+
+    @Override
+    public String toString() {
+        return "FindBugs{" + '}';
+    }
+
+    public FindBugs() {
+    }
+    
+}
